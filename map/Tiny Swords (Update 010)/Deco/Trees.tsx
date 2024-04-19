f7e4e60728bdf7dcee9a2331803353b9f320c6e9
Mioxive <mioxive@outlook.com>
2024-04-19 20:16:29 +0700

    текстуры

---
+++ map/Tiny Swords (Update 010)/Deco/Trees.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="C:\Users\stasz\Downloads\Tiny Swords (Update 010)\Resources\Trees" tilewidth="253" tileheight="188" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="3">
+  <image width="253" height="188" source="../Resources/Trees/Tree1.png"/>
+ </tile>
+</tileset>
