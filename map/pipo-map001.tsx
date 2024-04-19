5a35cfc627d117d4c6b77f3c9c114d41c5d5465d
Mioxive <mioxive@outlook.com>
2024-04-19 16:27:12 +0700

    Карта

---
+++ map/pipo-map001.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="pipo-map001" tilewidth="48" tileheight="48" tilecount="136" columns="8">
+ <image source="D:/Pipoya RPG World Tileset 48x48 40x40 32x32 for Tiled1.5/48x48/pipo-map001.png" width="384" height="816"/>
+</tileset>
--- src/main.py
+++ src/main.py
@@ -7,31 +7,65 @@ import numpy as np
 class GameView(arcade.View):  # TODO player logic in arcade.Character
     def __init__(self):
         super().__init__()
+        self.scene = None
         self.player_sprite = None
         self.up_pressed = False
         self.down_pressed = False
         self.left_pressed = False
         self.right_pressed = False
         self.speed = 128
+        self.tiled_map = None
+        self.physics_engine = None
+        self.camera = None
 
     def setup(self):
-        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/femalePerson_idle.png", 1)
-        self.player_sprite.center_x = self.window.width/2
-        self.player_sprite.center_y = self.window.height/2
+        """ Настройки игры """
 
+        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/femalePerson_idle.png", 0.5)
+        self.player_sprite.center_x = self.window.width / 2
+        self.player_sprite.center_y = self.window.height / 2
+        self.tiled_map = arcade.load_tilemap("map/testmap.tmj", 1.5)
+        self.scene = arcade.Scene.from_tilemap(self.tiled_map)
+        self.camera = arcade.Camera(self.window.width, self.window.height)
+        self.scene.add_sprite("player", self.player_sprite)
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, walls=[self.scene["2"], self.scene["3"], self.scene["4"]])
+
+    def center_camera_to_player(self):
+        scr_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        scr_center_y = self.player_sprite.center_y - (self.camera.viewport_height / 2)
+
+        if scr_center_x < 0:
+            scr_center_x = 0
+        if scr_center_y < 0:
+            scr_center_y = 0
+
+        player_centered = [scr_center_x, scr_center_y]
+
+        self.camera.move_to(player_centered)
     def on_show_view(self):
+        """ Инициализация """
+
         self.setup()
         arcade.set_background_color(arcade.color.TEA_GREEN)
 
     def on_draw(self):
+        """ Отрисовка """
+
         self.clear()
+        self.scene.draw()
+        self.camera.use()
         self.player_sprite.draw()
 
     def on_update(self, delta_time):
+        """ Логика """
+
         self.player_sprite.center_x += self.player_sprite.change_x * delta_time * self.speed
         self.player_sprite.center_y += self.player_sprite.change_y * delta_time * self.speed
+        self.center_camera_to_player()
 
     def process_keychange(self):
+        """ Обработка нажатия клавиш """
+
         if self.up_pressed and not self.down_pressed:
             self.player_sprite.change_y = 1
         elif self.down_pressed and not self.up_pressed:
@@ -51,6 +85,8 @@ class GameView(arcade.View):  # TODO player logic in arcade.Character
             self.player_sprite.change_y /= total_velocity
 
     def on_key_press(self, symbol: int, modifiers: int):
+        """ Нажатие """
+
         if symbol == arcade.key.W:
             self.up_pressed = True
         elif symbol == arcade.key.S:
@@ -63,6 +99,8 @@ class GameView(arcade.View):  # TODO player logic in arcade.Character
         self.process_keychange()
 
     def on_key_release(self, symbol: int, modifiers: int):
+        """ Отпускание """
+
         if symbol == arcade.key.W:
             self.up_pressed = False
         elif symbol == arcade.key.S:
@@ -76,11 +114,15 @@ class GameView(arcade.View):  # TODO player logic in arcade.Character
 
 
 class GameWindow(arcade.Window):
+    """ Класс окна """
+
     def __init__(self, width, height, title):
         super().__init__(width, height, title, resizable=True)
 
 
 class Game:
+    """ Класс игры """
+
     def __init__(self, game_window):
         self.game_window = game_window
 
@@ -89,7 +131,8 @@ class Game:
         arcade.run()
 
 def main():
-    game_window = GameWindow(640, 400, "the game")
+
+    game_window = GameWindow(1600, 900, "the game")
     game = Game(game_window)
     game.run()
 
