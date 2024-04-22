d77b68dddbcf3bd9db249e882f5baa5b9038c552
Umadd <102500290+UltraMadd@users.noreply.github.com>
2024-04-22 08:59:10 +0000

    Revert "Menu (#16)"
    This reverts commit 52210aeccb26669cd22d7783186bc00cb679a590.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
 import arcade
-from arcade.experimental.uislider import UISlider
-from arcade.gui import UIManager, UIAnchorWidget, UILabel
-from arcade.gui.events import UIOnChangeEvent
-import arcade.gui
 import pyglet.math as gmath
 
 from entities.player import Player
 from entities.enemy import Enemy
+
+
 
 
 def is_point_in_rect(point_x, point_y, rect_x, rect_y, rect_w, rect_h):
@@ -88,10 +86,6 @@ class GameView(arcade.View):  # TODO player logic in arcade.Character
                     enemy_x_delta, enemy_y_delta = (player_pos - enemy_pos).normalize()
                     enemy.center_x += enemy_x_delta * delta_time * enemy.speed
                     enemy.center_y += enemy_y_delta * delta_time * enemy.speed
-        if self.player.hitpoints <= 0:
-            arcade.close_window()
-            DeadWindow()
-            arcade.run()
 
     def process_keychange(self):
         if self.up_pressed and not self.down_pressed:
@@ -113,10 +107,6 @@ class GameView(arcade.View):  # TODO player logic in arcade.Character
             self.player.change_y /= 1.5
 
     def on_key_press(self, symbol: int, modifiers: int):
-        if symbol == arcade.key.ESCAPE:
-            arcade.close_window()
-            MainWindow()
-            arcade.run()
         if symbol == arcade.key.W:
             self.up_pressed = True
         elif symbol == arcade.key.S:
@@ -142,207 +132,6 @@ class GameWindow(arcade.Window):
         super().__init__(width, height, title, resizable=True)
 
 
-class MainWindow(arcade.Window):
-    def __init__(self):
-        super().__init__(1920, 1080, title="Main window")
-
-        arcade.set_background_color(arcade.color.WHITE)
-
-        self.uimanager = arcade.gui.UIManager()
-        self.uimanager.enable()
-
-        self.set_location(0, 0)
-
-        start_button = arcade.gui.UIFlatButton(text="Start Game",
-                                               width=300,
-                                               height=75
-                                               )
-        settings_button = arcade.gui.UIFlatButton(text="Settings",
-                                                  width=300,
-                                                  height=75
-                                                  )
-        exit_button = arcade.gui.UIFlatButton(text="Exit",
-                                              width=300,
-                                              height=75
-                                              )
-
-        self.uimanager.add(
-            arcade.gui.UIAnchorWidget(
-                align_x=-790,
-                anchor_y="center_y",
-                child=start_button)
-        )
-
-        self.uimanager.add(
-            arcade.gui.UIAnchorWidget(
-                align_x=-790,
-                align_y=-100,
-                child=settings_button)
-        )
-        self.uimanager.add(
-            arcade.gui.UIAnchorWidget(
-                align_x=-790,
-                align_y=-200,
-                child=exit_button)
-        )
-
-        start_button.on_click = self.on_button_click
-
-        exit_button.on_click = self.exit
-
-        settings_button.on_click = self.settings
-
-    def exit(self, event):
-        arcade.exit()
-
-    def settings(self, event):
-        self.close()
-        Settings()
-        arcade.run()
-
-    def on_button_click(self, event):
-        arcade.close_window()
-        main()
-
-    def on_draw(self):
-        arcade.start_render()
-        self.uimanager.draw()
-
-
-class Settings(arcade.Window):
-    def __init__(self):
-        self.music = True
-        self.volume = 100
-        super().__init__(1920, 1080, title='Settings', resizable=True)
-
-        self.manager = UIManager()
-        self.manager.enable()
-
-        arcade.set_background_color(arcade.color.WHITE)
-
-        self.set_location(0, 0)
-
-        self.uimanager = arcade.gui.UIManager()
-        self.uimanager.enable()
-
-        screen_resolution = arcade.gui.UIFlatButton(text="Screen_resolution",
-                                                    width=300,
-                                                    height=75
-                                                    )
-        volume = arcade.gui.UIFlatButton(text="Volume",
-                                         width=300,
-                                         height=75
-                                         )
-        music = arcade.gui.UIFlatButton(text="Music",
-                                        width=300,
-                                        height=75
-                                        )
-        back = arcade.gui.UIFlatButton(text='Back',
-                                       width=300,
-                                       height=75
-                                       )
-
-        self.uimanager.add(
-            arcade.gui.UIAnchorWidget(
-                align_x=-250,
-                align_y=+200,
-                child=back)
-        )
-
-        self.uimanager.add(
-            arcade.gui.UIAnchorWidget(
-                align_x=+150,
-                align_y=+200,
-                child=volume)
-        )
-
-        self.uimanager.add(
-            arcade.gui.UIAnchorWidget(
-                align_x=-780,
-                align_y=+200,
-                child=screen_resolution)
-        )
-
-        self.uimanager.add(
-            arcade.gui.UIAnchorWidget(
-                align_x=+600,
-                align_y=+200,
-                child=music)
-        )
-        back.on_click = self.back
-
-        ui_slider = UISlider(value=100, width=300, height=50)
-        label = UILabel(text=f"{ui_slider.value:}")
-
-        @ui_slider.event()
-        def on_change(event: UIOnChangeEvent):
-            label.text = f"{ui_slider.value // 1:}"
-            label.fit_content()
-
-        self.manager.add(UIAnchorWidget(child=ui_slider, align_x=+150, align_y=+50))
-        self.manager.add(UIAnchorWidget(child=label, align_y=+100, align_x=+150))
-
-    def on_draw(self):
-        arcade.start_render()
-        self.uimanager.draw()
-        self.manager.draw()
-
-    def back(self, event):
-        self.close()
-        r = MainWindow()
-        r.width = self.width
-        r.height = self.height
-        arcade.run()
-
-
-class DeadWindow(arcade.Window):
-    def __init__(self):
-        super().__init__(1920, 1080, title="Main window")
-
-        arcade.set_background_color(arcade.color.WHITE)
-
-        self.uimanager = arcade.gui.UIManager()
-        self.uimanager.enable()
-
-        self.set_location(0, 0)
-
-        start_button = arcade.gui.UIFlatButton(text="Start Game",
-                                               width=300,
-                                               height=75
-                                               )
-        main_menu = arcade.gui.UIFlatButton(text='To main menu',
-                                            width=300,
-                                            height=75
-                                            )
-
-        self.uimanager.add(
-            arcade.gui.UIAnchorWidget(
-                child=start_button)
-        )
-
-        self.uimanager.add(
-            arcade.gui.UIAnchorWidget(
-                child=main_menu, align_y=-100
-            )
-        )
-
-        start_button.on_click = self.on_button_click
-        main_menu.on_click = self.to_main
-
-    def to_main(self, event):
-        arcade.close_window()
-        MainWindow()
-        arcade.run()
-
-    def on_button_click(self, event):
-        arcade.close_window()
-        main()
-
-    def on_draw(self):
-        arcade.start_render()
-        self.uimanager.draw()
-
-
 class Game:
     def __init__(self, game_window):
         self.game_window = game_window
@@ -353,11 +142,11 @@ class Game:
 
 
 def main():
-    game_window = GameWindow(1920, 1080, "the game")
+    game_window = GameWindow(1600, 900, "the game")
     game = Game(game_window)
     game.run()
 
 
 if __name__ == '__main__':
-    MainWindow()
-    arcade.run()
+    main()
+
