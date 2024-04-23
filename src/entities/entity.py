52c88e21e8bb79791f5a272d7b8a4ccc6ec7e4ce
Mioxive <mioxive@outlook.com>
2024-04-23 21:57:33 +0700

    hp poloska :D

--- src/entities/entity.py
+++ src/entities/entity.py
@@ -4,7 +4,9 @@ import arcade
 class Entity(arcade.Sprite):
     def __init__(self, *args, hitpoints=100, **kwargs):
         super().__init__(*args, **kwargs)
+        self.max_hitpoints = hitpoints
         self.hitpoints = hitpoints
+        self.hit_box_algorithm = "Detailed"
 
     def damage(self, amount: int):
         self.hitpoints -= amount
