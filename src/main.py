d95985109050c7a6dcc116ae255faeced19e4901
UltraMadd <102500290+UltraMadd@users.noreply.github.com>
2024-05-04 21:47:56 +0700

    Fixed some issues and refactored

--- src/main.py
+++ src/main.py
@@ -1,26 +1,15 @@
 import arcade
-from views.game_view import GameView
 
-
-class GameWindow(arcade.Window):
-    def __init__(self, width, height, title):
-        super().__init__(width, height, title, resizable=True)
-
-
-class Game:
-    def __init__(self, game_window):
-        self.game_window = game_window
-
-    def run(self):
-        self.game_window.show_view(GameView())
-        arcade.run()
+from views.game_view import GameView, MainWindow
 
 
 def main():
-    game_window = GameWindow(1600, 900, "the game")
-    game = Game(game_window)
-    game.run()
+    game_window = arcade.Window(1920, 1080, resizable=True)
+    menu_view = MainWindow()
+    game_window.show_view(menu_view)
+    arcade.run()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
